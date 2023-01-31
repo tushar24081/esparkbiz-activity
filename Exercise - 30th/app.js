99865abbab696e09e99f23f7bf6eadcce219a1b0
tushar24081 <tushar24081@gmail.com>
2023-01-31 13:28:22 +0530

    Changes made into cube game

--- Exercise - 30th/app.js
+++ Exercise - 30th/app.js
@@ -1,36 +1,62 @@
 const tableContainer = document.getElementById("table-container")
 var tusharRows = 3;
 var tusharCols = 3;
+var tusharOpacityCounter = 0.3
+tusharResponsibleTimer();
+function tusharResponsibleTimer() {
+    var tusharTargetDate = new Date().getTime() + (1 * 60 * 1000);
+    intervalX = setInterval(function () {
+        var tusharNow = new Date().getTime();
+        var tusharDistance = tusharTargetDate - tusharNow;
+
+        if (tusharDistance < 0) {
+            clearInterval(intervalX)
+            tusharResponsibleTimer();
+            tusharDrawCounter += 1;
+            tusharGameOver = true;
+            checkIsOver();
+            refershWinner();
+        }
+        var tusharMinutes = Math.floor((tusharDistance % (1000 * 60 * 60)) / (1000 * 60));
+        var tusharSeconds = Math.floor((tusharDistance % (1000 * 60)) / (1000));
+        timer.innerHTML = `Timer: ${tusharMinutes} : ${tusharSeconds}`;
+
+
+    }, 1000);
+}
 generatingTable(tusharRows, tusharCols)
 
 function generatingTable(tusharRows, tusharCols) {
     if(tusharCols > 10 && tusharRows > 10){
         alert("Game Over")
+        window.location.reload();
         return;
     }
-    var table = document.createElement("table");
+    var Tushartable = document.createElement("table");
     for (let i = 0; i < tusharRows; i++) {
         let tr = document.createElement("tr");
         for (let j = 0; j < tusharCols; j++) {
             td = document.createElement("td");
             tr.appendChild(td);
         }
-        table.appendChild(tr);
+        Tushartable.appendChild(tr);
     }
 
-    document.getElementById("table-container").appendChild(table)
+    document.getElementById("table-container").appendChild(Tushartable)
 
     var tusharTds = document.querySelectorAll("td");
     var tusharRandomNumber = Math.floor(Math.random() * tusharTds.length);
-    var r = Math.floor(Math.random() * 255);
-    var g = Math.floor(Math.random() * 255);
-    var b = Math.floor(Math.random() * 255);
+    var tusharR = Math.floor(Math.random() * 255);
+    var tusharG = Math.floor(Math.random() * 255);
+    var tusharB = Math.floor(Math.random() * 255);
     tusharTds.forEach((td) => {
-        td.style.background = `rgba(${r}, ${g}, ${b})`
+        td.style.background = `rgba(${tusharR}, ${tusharG}, ${tusharB})`
     })
-    tusharTds[tusharRandomNumber].style.background = `rgba(${r}, ${g}, ${b}, 0.5)`;
+    tusharTds[tusharRandomNumber].style.background = `rgba(${tusharR}, ${tusharG}, ${tusharB}, ${tusharOpacityCounter})`;
     tusharTds[tusharRandomNumber].addEventListener("click", () => {
-        table.remove();
+        Tushartable.remove();
+        tusharOpacityCounter += 0.1
+        console.log(tusharOpacityCounter);
         generatingTable(++tusharRows, ++tusharCols)
     })
 
